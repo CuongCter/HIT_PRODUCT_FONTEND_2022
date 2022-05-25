487110842945f0498eed1231755ac2b84d6f095a
CuongCter <cuongcter442002@gmail.com>
2022-05-25 15:46:37 +0700

    project

---
+++ font_end/travel/src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header__menu'>
+                <div className='header__menu-item'></div>
+                <div ></div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Header
