07dae0539ef37727eac51d64c90d7d121d80854c
VuQuyh <vuthiquynh2002hy@gmail.com>
2022-05-26 20:22:29 +0700

    footer

--- font_end/travel/src/pages/Home.jsx
+++ font_end/travel/src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Footer from '../components/footer/Footer'
 import Header from '../components/header/Header'
 const Home = () => {
     return (
         <div>
-            <Header />
+            <Footer />
         </div>
     )
 }
