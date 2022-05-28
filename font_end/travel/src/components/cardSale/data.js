6fe9d2d97b9c46d6712ab25547a99a1ad239ac29
CuongCter <cuongcter442002@gmail.com>
2022-05-29 00:49:42 +0700

    [Week1] cuong coding slick

---
+++ font_end/travel/src/components/cardSale/data.js
@@ -0,0 +1,58 @@
+export const dataBestSeller = [
+    {
+        id: 1,
+        title: "Khách sạn Carevell",
+        location: "Sài Gòn",
+        price: "1494000",
+        linkImg: "https://pix10.agoda.net/hotelImages/2817185/-1/4406a970306a452300f94532410dab2c.jpg?ca=10&ce=1&s=1024x768"
+    },
+    {
+        id: 2,
+        title: "Khách sạn Carevell",
+        location: "Sài Gòn",
+        price: "1494000",
+        linkImg: "https://pix10.agoda.net/hotelImages/2817185/-1/4406a970306a452300f94532410dab2c.jpg?ca=10&ce=1&s=1024x768"
+    },
+    {
+        id: 3,
+        title: "Khách sạn Carevell",
+        location: "Sài Gòn",
+        price: "1494000",
+        linkImg: "https://pix10.agoda.net/hotelImages/2817185/-1/4406a970306a452300f94532410dab2c.jpg?ca=10&ce=1&s=1024x768"
+    },
+    {
+        id: 4,
+        title: "Khách sạn Carevell",
+        location: "Sài Gòn",
+        price: "1494000",
+        linkImg: "https://pix10.agoda.net/hotelImages/2817185/-1/4406a970306a452300f94532410dab2c.jpg?ca=10&ce=1&s=1024x768"
+    },
+    {
+        id: 5,
+        title: "Khách sạn Carevell",
+        location: "Sài Gòn",
+        price: "1494000",
+        linkImg: "https://pix10.agoda.net/hotelImages/2817185/-1/4406a970306a452300f94532410dab2c.jpg?ca=10&ce=1&s=1024x768"
+    },
+    {
+        id: 6,
+        title: "Khách sạn Carevell",
+        location: "Sài Gòn",
+        price: "1494000",
+        linkImg: "https://pix10.agoda.net/hotelImages/2817185/-1/4406a970306a452300f94532410dab2c.jpg?ca=10&ce=1&s=1024x768"
+    },
+    {
+        id: 7,
+        title: "Khách sạn Carevell",
+        location: "Sài Gòn",
+        price: "1494000",
+        linkImg: "https://pix10.agoda.net/hotelImages/2817185/-1/4406a970306a452300f94532410dab2c.jpg?ca=10&ce=1&s=1024x768"
+    },
+    {
+        id: 8,
+        title: "Khách sạn Carevell",
+        location: "Sài Gòn",
+        price: "1494000",
+        linkImg: "https://pix10.agoda.net/hotelImages/2817185/-1/4406a970306a452300f94532410dab2c.jpg?ca=10&ce=1&s=1024x768"
+    },
+]
