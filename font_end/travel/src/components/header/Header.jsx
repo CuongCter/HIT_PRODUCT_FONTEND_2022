380209ef4dd57d8d8fbd3bf588f6c01423b24cb4
CuongCter <cuongcter442002@gmail.com>
2022-06-01 00:59:58 +0700

    [Week2][Cuong] fixed screen slick, infoLocation moblie and change dataHeader

--- font_end/travel/src/components/header/Header.jsx
+++ font_end/travel/src/components/header/Header.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import { Link, animateScroll as scroll, } from 'react-scroll'
-
-
 import { MenuIcon, XIcon } from '@heroicons/react/outline';
 import './Header.scss'
+import { dataHeader } from './dataHeader';
+
 const Navbar = () => {
     const [nav, setNav] = useState(false)
     const handleClick = () => setNav(!nav)
@@ -15,14 +15,11 @@ const Navbar = () => {
             <div className='w-screen h-[90px] z-10 bg-zinc-200 fixed drop-shadow-lg '>
                 <div className='px-10 flex justify-between items-center w-full h-full '>
                     <div className='flex items-center '>
-                        <h1 className='text-3xl font-bold mr-4 sm:text-3xl'>HelloTravel</h1>
-                        <ul className='hidden md:flex md:ml-44 text-lg  font-normal'>
-                            <li className=''><Link to="home" smooth={true} duration={500} className='text-black'>Trang chủ</Link></li>
-                            <li><Link to="\about" smooth={true} offset={-200} duration={500} className='text-black'>Địa điểm</Link></li>
-                            <li><Link to="\support" smooth={true} offset={-50} duration={500} className='text-black'>Dịch vụ</Link></li>
-                            <li><Link to="\platforms" smooth={true} offset={-100} duration={500} className='text-black'>Blog</Link></li>
-                            <li><Link to="\pricing" smooth={true} offset={-50} duration={500} className='text-black'>Tin tức</Link></li>
-                            <li><Link to="\contact" smooth={true} offset={-50} duration={500} className='text-black'>Liên hệ</Link></li>
+                        <h1 className='text-3xl font-bold xl:mr-12 sm:text-3xl'>HelloTravel</h1>
+                        <ul className='hidden md:flex md:ml-44 text-lg  font-normal '>
+                            {dataHeader.map((item) => (
+                                <li className='text-black font-medium'>{item.title}</li>
+                            ))}
                         </ul>
                     </div>
                     <div className='hidden md:flex pr-4 lg:hidden xl:block'>
@@ -37,12 +34,10 @@ const Navbar = () => {
                 </div>
 
                 <ul className={!nav ? 'hidden' : 'absolute bg-zinc-200 w-full px-8'}>
-                    <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="\home" smooth={true} duration={500} className='text-black'>Trang chủ</Link></li>
-                    <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="\about" smooth={true} offset={-200} duration={500} className='text-black'>Địa điểm</Link></li>
-                    <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="\support" smooth={true} offset={-50} duration={500} className='text-black'>Dịch vụ</Link></li>
-                    <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="\platforms" smooth={true} offset={-100} duration={500} className='text-black'>Blog</Link></li>
-                    <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="\pricing" smooth={true} offset={-50} duration={500} className='text-black'>Tin tức</Link></li>
-                    <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="\contact" smooth={true} offset={-50} duration={500} className='text-black'>Liên hệ</Link></li>
+                    {dataHeader.map((item) => (
+                        <li className='text-black font-medium border-b-2 border-zinc-300 w-full'>{item.title}</li>
+                    ))}
+
                     <div className='flex flex-col my-4'>
                         <button className='bg-transparent text-indigo-600 px-8 py-3 mb-4 '>Sign In</button>
                         <button className='px-8 py-3'>Sign Up</button>
--- font_end/travel/src/components/infoLocation/InfoLocation.jsx
+++ font_end/travel/src/components/infoLocation/InfoLocation.jsx
@@ -1,26 +1,31 @@
 import React from 'react'
 import './InfoLocation.scss'
-const InfoLocation = () => {
+
+
+const InfoLocation = (dataLoca) => {
     return (
-        <div className='infoLocation'>
-            <div className='infoLocation__content'>
+        <div className='infoLocation '>
+            <div className='infoLocation__content '>
                 <div className='infoLocation__content_header'>
                     <div className='infoLocation__content_header-title'>
-                        <h1 className='text-2xl'>Các điểm đến hấp dẫn</h1>
-                        <h2>Các địa điểm du lịch hàng đầu do HelloTravel đề xuất</h2>
+                        <h1 className='xl:text-2xl xl:font-semibold md:font-semibold md:text-2xl sm:text-xl sm:font-semibold'>Các điểm đến hấp dẫn</h1>
+                        <h2 className='xl:text-lg md:text-lg sm:text-xs'>Các địa điểm du lịch hàng đầu do HelloTravel đề xuất </h2>
                     </div>
                     <div className='infoLocation__content_header-btn '>
                         <button className="px-5 py-2 text-pink-400 back bg-white border-pink-400 ">Khám phá ngay</button>
                     </div>
 
                 </div>
-                <div className='infoLocation__content_main grid grid-cols-4 gap-4'>
-                    <div className='infoLocation__content_main-pic row-span-2 min-h-[390px] min-w-[285px]'></div>
-                    <div className='infoLocation__content_main-pic min-h-[178px] min-w-[285px]'></div>
-                    <div className='infoLocation__content_main-pic row-span-2 min-h-[390px] min-w-[285px]'></div>
-                    <div className='infoLocation__content_main-pic min-h-[178px] min-w-[285px]'></div>
-                    <div className='infoLocation__content_main-pic min-h-[178px] min-w-[285px]'></div>
-                    <div className='infoLocation__content_main-pic min-h-[178px] min-w-[285px]'></div>
+
+                <div className='infoLocation__content_main xl:grid xl:grid-cols-4 xl:gap-4 lg:grid lg:grid-cols-3 lg:gap-4 md:mt-3 sm:grid'>
+                    <div className='infoLocation__content_main-pic xl:row-span-2 xl:min-h-[186px] xl:min-w-[285px] lg:row-span-2 lg:min-h-[180px]'>
+                        <img src="/font_end/travel/public/imgs/hanoi2.png" alt="" />
+                    </div>
+                    <div className='infoLocation__content_main-pic xl:row-span-1 xl:min-h-[186px] xl:min-w-[286px] lg:row-span-2 lg:min-h-[180px]'>2</div>
+                    <div className='infoLocation__content_main-pic xl:row-span-2 xl:min-h-[186px] xl:min-w-[285px] lg:row-span-2 lg:min-h-[180px]'>3</div>
+                    <div className='infoLocation__content_main-pic  xl:row-span-1 xl:min-h-[186px] xl:min-w-[285px] lg:row-span-2 lg:min-h-[180px]'>4</div>
+                    <div className='infoLocation__content_main-pic  xl:row-span-1 xl:min-h-[186px] xl:min-w-[285px] lg:row-span-2 lg:min-h-[180px]'>5</div>
+                    <div className='infoLocation__content_main-pic  xl:row-span-1 xl:min-h-[186px] xl:min-w-[285px] lg:row-span-2 lg:min-h-[180px]'>6</div>
                 </div>
             </div>
 
