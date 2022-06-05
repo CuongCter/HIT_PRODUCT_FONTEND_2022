fc8c1c715697ac30000e08ba16734c119ee18cdd
VuQuyh <vuthiquynh2002hy@gmail.com>
2022-06-05 11:16:27 +0700

    []

---
+++ font_end/travel/src/MeetUs/dataMeetUs.js
@@ -0,0 +1,50 @@
+export const dataUserMeetUs = [
+  {
+     id: '1',
+     imgCover: 'https://meta.vn/Data/image/2022/01/13/anh-dep-thien-nhien-3.jpg',
+     imgAvt: 'https://kenh14cdn.com/203336854389633024/2021/7/29/myvanh2054286994949691217410844763204888409440211n-1627523268551486434209.jpg',
+     name: 'Mr.Vân',
+     comment: 'Sự hài lòng của các bạn là niềm vui của chúng tôi',
+     linkFB: 'https://www.facebook.com/vuquyh202/',
+     linkIns: 'https://www.instagram.com/quynh2974/'
+  },
+  {
+     id: '2',
+     imgCover: 'https://meta.vn/Data/image/2022/01/13/anh-dep-thien-nhien-3.jpg',
+     imgAvt: 'https://kenh14cdn.com/203336854389633024/2021/7/29/myvanh2054286994949691217410844763204888409440211n-1627523268551486434209.jpg',
+     name: 'Mr.Vân',
+     comment: 'Sự hài lòng của các bạn là niềm vui của chúng tôi',
+     linkFB: 'https://www.facebook.com/vuquyh202/',
+     linkIns: 'https://www.instagram.com/quynh2974/'
+  },
+  {
+     id: '3',
+     imgCover: 'https://meta.vn/Data/image/2022/01/13/anh-dep-thien-nhien-3.jpg',
+     imgAvt: 'https://kenh14cdn.com/203336854389633024/2021/7/29/myvanh2054286994949691217410844763204888409440211n-1627523268551486434209.jpg',
+     name: 'Mr.Vân',
+     comment: 'Sự hài lòng của các bạn là niềm vui của chúng tôi',
+     linkFB: 'https://www.facebook.com/vuquyh202/',
+     linkIns: 'https://www.instagram.com/quynh2974/'
+  },
+  {
+     id: '4',
+     imgCover: 'https://meta.vn/Data/image/2022/01/13/anh-dep-thien-nhien-3.jpg',
+     imgAvt: 'https://kenh14cdn.com/203336854389633024/2021/7/29/myvanh2054286994949691217410844763204888409440211n-1627523268551486434209.jpg',
+     name: 'Mr.Vân',
+     comment: 'Sự hài lòng của các bạn là niềm vui của chúng tôi',
+     linkFB: 'https://www.facebook.com/vuquyh202/',
+     linkIns: 'https://www.instagram.com/quynh2974/'
+  },
+  {
+     id: '5',
+     imgCover: 'https://meta.vn/Data/image/2022/01/13/anh-dep-thien-nhien-3.jpg',
+     imgAvt: 'https://kenh14cdn.com/203336854389633024/2021/7/29/myvanh2054286994949691217410844763204888409440211n-1627523268551486434209.jpg',
+     name: 'Mr.Vân',
+     comment: 'Sự hài lòng của các bạn là niềm vui của chúng tôi',
+     linkFB: 'https://www.facebook.com/vuquyh202/',
+     linkIns: 'https://www.instagram.com/quynh2974/'
+     }
+  
+     
+  
+ ]
