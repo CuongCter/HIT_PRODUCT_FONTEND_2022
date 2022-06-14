954447dbd638d29dbf18e9b720bdead67805323a
VuQuyh <vuthiquynh2002hy@gmail.com>
2022-06-14 23:58:53 +0700

    [week3][Quynh]fixed locations

--- font_end/travel/src/pages/Home.jsx
+++ font_end/travel/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React, { useRef, useState } from "react";
 import Footer from '../components/footer/Footer'
 import Header from '../components/header/Header'
 import CardSale from '../components/cardSale/CardSale'
-import MeetUs from '../components/MeetUs/MeetUs'
+import MeetUs from '../components/meetUs/MeetUs'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Navigate, useNavigate } from 'react-router-dom'
 import InfoLocation from '../components/infoLocation/InfoLocation'
@@ -12,11 +12,8 @@ import CommentCard from '../components/comment/CommentCard'
 import CardHotel from '../components/cardHotel/CardHotel'
 import ListHotel from '../components/listHotel/ListHotel'
 import Blogs from '../components/blogs/Blogs'
-<<<<<<< Updated upstream
-=======
-import CardInfo from '../components/cardInfo/CardInfo'
 
->>>>>>> Stashed changes
+import CardInfo from '../components/cardInfo/CardInfo'
 
 
 const Home = () => {
